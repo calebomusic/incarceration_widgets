65bf84dd77919f0f920e8b2c53f98ff878692dee
calebomusic <calebomusic@gmail.com>
2017-04-01 14:16:21 -0700

    Git init with first graph

---
+++ lib/reactIndex.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ReactDom from 'react-dom';
+
+import GraphWidget from './generateGraphWidget.jsx';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  // ReactDom.render(<GraphWidget options={} graphId={'test'}/>,root)
+});
